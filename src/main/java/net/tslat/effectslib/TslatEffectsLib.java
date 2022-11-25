b6868cd3b57479aaa6bc2ac87f3a9217433e5e7a
Tslat <tslatmail@gmail.com>
2022-11-25 16:40:22 +1000

    Update to 1.2

--- src/main/java/net/tslat/effectslib/TslatEffectsLib.java
+++ src/main/java/net/tslat/effectslib/TslatEffectsLib.java
@@ -8,7 +8,7 @@ import static net.tslat.effectslib.TslatEffectsLib.MOD_ID;
 
 @Mod(MOD_ID)
 public class TslatEffectsLib {
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.2";
 	public static final String MOD_ID = "tslateffectslib";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
