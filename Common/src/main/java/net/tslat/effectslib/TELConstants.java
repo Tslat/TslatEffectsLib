e1a3d5870a40347188d2fa7723e8787d8a26dfd5
Tslat <tslatmail@gmail.com>
2022-12-18 14:58:01 +1000

    Some minor fixes and consistency changes

--- Common/src/main/java/net/tslat/effectslib/TELConstants.java
+++ Common/src/main/java/net/tslat/effectslib/TELConstants.java
@@ -3,8 +3,8 @@ package net.tslat.effectslib;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Constants {
-	public static final String VERSION = "1.3";
+public class TELConstants {
+	public static final String VERSION = "1.4.1";
 	public static final String MOD_ID = "tslateffectslib";
 	public static final String MOD_NAME = "TslatEffectsLib";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
--- Forge/src/main/java/net/tslat/effectslib/TslatEffectsLib.java
+++ Forge/src/main/java/net/tslat/effectslib/TslatEffectsLib.java
@@ -2,5 +2,5 @@ package net.tslat.effectslib;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(Constants.MOD_ID)
+@Mod(TELConstants.MOD_ID)
 public class TslatEffectsLib {}
